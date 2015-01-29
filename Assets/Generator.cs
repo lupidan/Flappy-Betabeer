3f8b6ff6365006845d71f25377c2eba0028568dd
Daniel Lupiañez Casares <lupidan@gmail.com>
2015-01-29 13:29:27 +0100

    Created Generator. Added Generator Code to create Obstacle Prefab clones every 2 seconds

---
+++ Assets/Generator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+	public GameObject obstaclePrefab = null;
+	
+	private void CreateObstacle() {
+		if (GameControl.DefaultControl.status == GameControl.Status.Playing)
+			Instantiate(this.obstaclePrefab, this.transform.position, Quaternion.identity);
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("CreateObstacle", 0.0f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
