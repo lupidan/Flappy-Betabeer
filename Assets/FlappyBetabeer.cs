9826c58d7d7663138f7543f5e2c3cf8f7ea82e76
Daniel Lupiañez Casares <lupidan@gmail.com>
2015-01-29 13:07:15 +0100

    Added animation for FlappyBetabeer

--- Assets/FlappyBetabeer.cs
+++ Assets/FlappyBetabeer.cs
@@ -13,6 +13,14 @@ public class FlappyBetabeer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		//Rotation code
+		if (GameControl.DefaultControl.status == GameControl.Status.Playing) {
+			float zAngle = this.rigidbody2D.velocity.y * 10.0f;
+			zAngle = Mathf.Clamp(zAngle, -45.0f, 45.0f);
+			this.transform.rotation = Quaternion.Euler(0.0f,0.0f,zAngle);
+		}
+		
+	
 	}
 	
 	
