7b2f0c47623783b956cbf721f3f882bff8a0d079
Daniel Lupiañez Casares <lupidan@gmail.com>
2015-01-29 13:40:27 +0100

    Added scroll script to all planes.

---
+++ Assets/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	public Vector2 scrollSpeed = Vector2.zero;
+	
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameControl.DefaultControl.status == GameControl.Status.Playing)
+			this.renderer.material.mainTextureOffset += this.scrollSpeed * Time.deltaTime;
+	}
+}
